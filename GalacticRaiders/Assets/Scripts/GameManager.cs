ba8276c48c7e37762332030bdaa43c39defddd70
MondalAn <mondal.an@northeastern.edu>
2024-06-03 13:03:48 -0400

    Health UI and scripting in GameManager
    Updated GameManager to take damage and heal, and added the necessary UI elements

--- GalacticRaiders/Assets/Scripts/GameManager.cs
+++ GalacticRaiders/Assets/Scripts/GameManager.cs
@@ -6,30 +6,78 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public float totalCurrency; // tracks the player's currency throughout levels
-    public float levelCurrency; // track the currency collected this level
-    public Text currencyText;
+    public int totalCurrency; // tracks the player's currency throughout levels
+    public int levelCurrency; // track the currency collected this level
+    public Text currencyText; // currency text element
+    public int maxHealth = 100; // maximum health of player
+    public int currentHealth; // current health of player
+    public Image healthFill; // health UI element
+    public Text healthText; // health text element
+    public GameObject[] inventory; // Weapon, ammo, and health inventory
 
     // Start is called before the first frame update
     void Start()
     {
         levelCurrency = 0;
-        DontDestroyOnLoad(gameObject);
         UpdateCurrencyText();
+        currentHealth = maxHealth;
+        UpdateHealthUI();
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentHealth == 0)
+        {
+            GameOver();
+        }
     }
 
+    // Increases level currency
     public void IncreaseCurrency(int amt) {
         levelCurrency += amt;
         UpdateCurrencyText();
     }
 
+    // Updates currency text
     void UpdateCurrencyText() {
         currencyText.text = "Currency: " + levelCurrency;
+    }
+
+    // Taking damage
+    public void Damage(int amt)
+    {
+        currentHealth -= amt;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthUI();
+
+    }
+
+    // Heal HP
+    public void Heal(int amt)
+    {
+        currentHealth += amt;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthUI();
+    }
+
+    // Updtes health UI
+    void UpdateHealthUI()
+    {
+        healthFill.fillAmount = (float)currentHealth / maxHealth;
+        healthText.text = currentHealth.ToString();
+    }
+
+    // Game over
+    public void GameOver()
+    {
+        Debug.Log("HP 0, game lost.");
+    }
+
+    // Level beat
+    public void LevelBeat()
+    {
+
     }
 }
