2203d425032cdaaaa009478cb3eb45c6ec03c823
qbn-benenson <sam.benenson@gmail.com>
2024-06-20 01:48:42 -0400

    pause menu
    CRUCIAL FIX: can still move mouse if not starting from main menu

--- GalacticRaiders/Assets/Scripts/PlayerControl/MouseLook.cs
+++ GalacticRaiders/Assets/Scripts/PlayerControl/MouseLook.cs
@@ -12,6 +12,7 @@ public class MouseLook : MonoBehaviour
 
     float xRotation;
     float yRotation;
+    float playerSens;
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +20,20 @@ public class MouseLook : MonoBehaviour
         // remove cursor from screen
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        if (GameManager.sensitivity != 0) {
+            playerSens = GameManager.sensitivity;
+        } else {
+            playerSens = 1;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         // get input from the mouse
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX * GameManager.sensitivity;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY * GameManager.sensitivity;
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX * playerSens;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY * playerSens;
 
         yRotation += mouseX;
         xRotation -= mouseY;
